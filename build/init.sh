7225898bb63bbf8dbde6a2f194ac3bbe2eed6c4f
jimonreal <jmonreal@gmail.com>
2018-08-11 23:50:05 +0100

    Adding checkout of revision that works

--- build/init.sh
+++ build/init.sh
@@ -15,6 +15,7 @@ mkdir /usr/local/sumo -p
 git clone --recursive https://github.com/eclipse/sumo.git
 
 cd sumo
+git checkout 7b378d18a18b4696bd84ea907adf10d6da8b4d97
 make -f Makefile.cvs
 ./configure --prefix=/usr/local/sumo
 make
